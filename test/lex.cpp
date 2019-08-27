e3e9a847a55cf7566595d1776619aeb32b19ebb5
Phil Eaton <philneaton95@gmail.com>
2019-08-26 21:16:07 -0400

    Tests work!

--- test/lex.cpp
+++ test/lex.cpp
@@ -1,21 +1,49 @@
 #include "gtest/gtest.h"
 
+extern "C" {
 #include "slowjs/lex.h"
+}
 
-TEST(lex, lex) {
-  const char *raw_source = "function main() { return a + 1; }";
-  vector_char source;
+TEST(lex, addition) {
+  const char *raw_source = "a + 1";
+  vector_char source = {0};
   vector_char_copy(&source, (char *)raw_source, sizeof(raw_source));
 
-  vector_token tokens;
+  vector_token tokens = {0};
   lex_error err = lex(source, &tokens);
 
   ASSERT_EQ(E_LEX_OK, err);
-  ASSERT_EQ(11, tokens.index);
+
+  const char *expected[] = {
+      "a",
+      "+",
+      "1",
+  };
+
+  ASSERT_EQ(sizeof expected / sizeof expected[0], tokens.index + 1);
+
+  int i;
+  for (i = 0; i < tokens.index; i++) {
+    vector_char_push(&tokens.elements[i].string, 0);
+    ASSERT_STREQ(expected[i], tokens.elements[i].string.elements);
+  }
+}
+
+TEST(lex, function) {
+  const char *raw_source = "function main() { return a + 1; }";
+  vector_char source = {0};
+  vector_char_copy(&source, (char *)raw_source, sizeof(raw_source));
+
+  vector_token tokens = {0};
+  lex_error err = lex(source, &tokens);
+
+  ASSERT_EQ(E_LEX_OK, err);
 
   const char *expected[] = {
       "function", "main", "(", ")", "{", "return", "a", "+", "1", ";", "}",
   };
+
+  ASSERT_EQ(sizeof expected / sizeof expected[0], tokens.index + 1);
 
   int i;
   for (i = 0; i < tokens.index; i++) {
